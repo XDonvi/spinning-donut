f65bd488eddf1b48d3d7bd507896b2c5f43bc923
XDonvi <eugenio.donvito5@gmail.com>
2023-06-26 19:51:22 +0200

    exctbl & compiling infos added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spinning-donut
 ASCII Art program of a spinning donut
+
+for compiling:
+$ gcc -lpthread -std=gnu99 -lm donut.c -o donut
--- donut.c
+++ donut.c
@@ -1,4 +1,9 @@
+#define _DEFAULT_SOURCE
+#define _XOPEN_SOURCE   600
+#define _POSIX_C_SOURCE 200112L
+
 #include<stdio.h>
+#include<unistd.h>
 #include<string.h>
 #include<math.h>
 
